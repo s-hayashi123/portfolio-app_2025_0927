8d83af4b43cef9e6fa984e10b8c8198c7d887034
s-hayashi123 <shake7602@gmail.com>
2025-09-28 15:55:59 +0900

    auth provider add

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,3 +1,15 @@
 import { createAuthClient } from "better-auth/react";
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
+
+export const googleSignIn = async () => {
+  const data = await signIn.social({
+    provider: "google",
+  });
+};
+
+export const githubSignIn = async () => {
+  const data = await signIn.social({
+    provider: "github",
+  });
+};
