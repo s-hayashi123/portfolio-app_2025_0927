16adc183af47091a15bb7637063a09604dfdff23
s-hayashi123 <shake7602@gmail.com>
2025-10-02 00:02:05 +0900

    Convert dashboard page from client to server component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,17 @@
-"use client";
+"use server";
 
-import { useRouter } from "next/navigation";
-import { useSession, signOut } from "@/lib/auth-client";
-import { useEffect } from "react";
+import { auth } from "@/lib/auth";
 import { getBookmarks } from "../actions/bookmarks";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { handleSignOut } from "@/lib/auth-client";
 
-export default function DashboardPage() {
-  const router = useRouter();
-  const { data: session, isPending } = useSession();
-
-  useEffect(() => {
-    if (!isPending && !session?.user) {
-      router.push("/sign-in");
-    }
-  }, [isPending, session, router]);
-
-  if (isPending)
-    return <p className="text-center mt-8 text-white">Loading...</p>;
-  if (!session?.user)
-    return <p className="text-center mt-8 text-white">Redirecting...</p>;
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session?.user) redirect("/");
 
   const { user } = session;
-
-  // const bookmarks = await getBookmarks();
+  const bookmarks = await getBookmarks();
 
   return (
     <div>
@@ -42,10 +31,7 @@ export default function DashboardPage() {
             <div className="hidden sm:block text-sm text-gray-400">
               {user.email}
             </div>
-            <button
-              onClick={() => signOut()}
-              className="bg-white text-black font-medium rounded-lg px-4 py-2 hover:bg-gray-300 transition-colors duration-200 text-sm cursor-pointer"
-            >
+            <button className="bg-white text-black font-medium rounded-lg px-4 py-2 hover:bg-gray-300 transition-colors duration-200 text-sm cursor-pointer">
               Sign Out
             </button>
           </div>
@@ -54,14 +40,14 @@ export default function DashboardPage() {
 
       <main className="max-w-md flex items-center justify-center flex-col mx-auto p-6 space-y-4 text-white">
         <h2>あなたのBookmark</h2>
-        {/* <ul>
+        <ul>
           {bookmarks?.map((bookmark) => (
             <li key={bookmark.id}>
               {bookmark.title}
               <a href={bookmark.url}>{bookmark.url}</a>
             </li>
           ))}
-        </ul> */}
+        </ul>
       </main>
     </div>
   );
