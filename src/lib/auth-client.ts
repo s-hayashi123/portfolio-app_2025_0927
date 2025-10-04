71d3af13f0d874676f11c06177b9f0cf3d64be0e
s-hayashi123 <shake7602@gmail.com>
2025-10-04 13:51:18 +0900

    add NEXT_PUBLIC_BASE_URL auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
-export const { signIn, signUp, signOut, useSession } = createAuthClient();
+export const { signIn, signUp, signOut, useSession } = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+});
 
 export const googleSignIn = () => {
   signIn.social({
